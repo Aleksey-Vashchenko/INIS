5f5a6f84050f972a92c8409b8f382d310bb36535
Aleksey_Vashchenko <leha.vashchenko@gmail.com>
2024-10-24 12:43:06 +0300

    refactor

--- LR_1/scripts.js
+++ LR_1/scripts.js
@@ -9,39 +9,33 @@ window.onload = function() {
     modal.style.display='none';
 
     shirts.forEach((shirt,index) => {
-        // Создание карточки для каждой футболки
+        
         const shirtCard = document.createElement('div');
         shirtCard.classList.add('shirt-card');
-
-        // Изображение футболки
+        
         const shirtImage = document.createElement('img');
         shirtImage.src = shirt.colors.white.front;
         shirtImage.alt = shirt.name;
         shirtImage.classList.add('shirt-image');
         shirtCard.appendChild(shirtImage);
-
-        // Название футболки
+        
         const shirtName = document.createElement('h2');
         shirtName.textContent = shirt.name;
         shirtCard.appendChild(shirtName);
 
-        // Доступные цвета
         const colorInfo = document.createElement('p');
         const colorCount = Object.keys(shirt.colors).length;
         colorInfo.textContent = `Available in ${colorCount} color${colorCount > 1 ? 's' : ''}`;
         shirtCard.appendChild(colorInfo);
 
-        // Контейнер для кнопок
         const buttonsContainer = document.createElement('div');
         buttonsContainer.classList.add('buttons-container');
 
-        // Кнопка "Quick View"
         const quickViewButton = document.createElement('button');
         quickViewButton.textContent = 'Quick View';
         quickViewButton.classList.add('btn');
         buttonsContainer.appendChild(quickViewButton);
 
-        // Кнопка "See Page"
         const seePageButton = document.createElement('button');
         seePageButton.textContent = 'See Page';
         seePageButton.classList.add('btn');
@@ -50,7 +44,6 @@ window.onload = function() {
         shirtCard.appendChild(buttonsContainer);
         shirtsList.appendChild(shirtCard);
 
-        // Обработчик для Quick View
         quickViewButton.addEventListener('click', () => {
             modal.style.display = 'block';
             modalShirtImage.src = shirt.default.front;
@@ -59,20 +52,18 @@ window.onload = function() {
             modalShirtPrice.textContent = shirt.price;
         });
         seePageButton.addEventListener('click', function() {
-            const shirtNumber = index; // Получаем имя футболки из data-атрибута кнопки
+            const shirtNumber = index; 
             const stringShirt = JSON.stringify(shirt);
             localStorage.setItem('selectedShirt', stringShirt);
-            window.location.href = '../LR_2/index.html'; // Переходим на страницу с деталями
+            window.location.href = '../LR_2/index.html'; 
             
         });
     });
-
-    // Закрытие модального окна
+    
     closeModal.addEventListener('click', () => {
         modal.style.display = 'none';
     });
 
-    // Закрытие модального окна при клике вне его
     window.addEventListener('click', (event) => {
         if (event.target == modal) {
             modal.style.display = 'none';
--- LR_2/scripts.js
+++ LR_2/scripts.js
@@ -1,23 +1,19 @@
 window.onload = function() {
-    // Получаем сохраненное имя футболки из localStorage
+    
     const storedShirt = localStorage.getItem('selectedShirt');
     const imagesPathPrefix = "../LR_1/";
 
-    // Текущее состояние
+    
     if (storedShirt) {
         const selectedShirt = JSON.parse(storedShirt);
         let currentColor = Object.keys(selectedShirt.colors)[0] || 'default';  
         let currentSide = 'front';
         
-        // Обновляем информацию на странице
         document.getElementById('shirt-name').textContent = selectedShirt.name;
         document.getElementById('shirt-price').textContent = selectedShirt.price;
         document.getElementById('shirt-description').textContent = selectedShirt.description;
-
-        // Устанавливаем картинку по умолчанию (фронтальная сторона)
         
 
-        // Добавляем обработчики для кнопок Front и Back
         document.getElementById('front-view').addEventListener('click', () => {
             currentSide = 'front';
             updateShirtImage(currentColor, currentSide,selectedShirt);
@@ -28,7 +24,6 @@ window.onload = function() {
             updateShirtImage(currentColor, currentSide,selectedShirt);
         });
 
-        // Генерируем кнопки цветов
         const colorOptionsDiv = document.getElementById('color-options');
         Object.keys(selectedShirt.colors).forEach(color => {
             const colorButton = document.createElement('button');
@@ -37,7 +32,6 @@ window.onload = function() {
             colorButton.classList.add('color-button');
             colorButton.setAttribute('data-color', color);
 
-            // Устанавливаем картинку соответствующего цвета при клике
             colorButton.addEventListener('click', (event) => {
                 currentColor = event.target.getAttribute('data-color');
                 updateShirtImage(currentColor, currentSide,selectedShirt);
@@ -49,12 +43,9 @@ window.onload = function() {
     }
 
     function updateShirtImage(currentColor, currentSide,selectedShirt) {
-        // Проверяем, есть ли такой цвет
         if (currentColor === "default") {
-            // Если цвета нет, используем изображение по умолчанию
             document.getElementById('shirt-image').src = imagesPathPrefix + selectedShirt.default[currentSide];
         } else {
-            // Устанавливаем картинку в зависимости от стороны (front/back) и цвета
             document.getElementById('shirt-image').src = imagesPathPrefix + selectedShirt.colors[currentColor][currentSide];
         }
     }
